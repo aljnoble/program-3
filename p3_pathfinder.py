045015b4cf366c8a71222aae7177341659195435
Alec Noble <aljnoble@ucsc.edu>
2015-04-19 12:17:54 -0700

    Implemented basic location of source box and destination box.

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -3,6 +3,16 @@ def find_path(src, dest, mesh):
     path = []
     visited = []
 
+    for box in mesh['boxes']:
+
+        if src[0] in range(box[0], box[1]):
+            if src[1] in range(box[2], box[3]):
+                src_box = box
+                print 'Source in box ' + str(src_box)
+        if dest[0] in range(box[0], box[1]):
+            if dest[1] in range(box[2], box[3]):
+                dest_box = box
+                print 'Destination in box ' + str(dest_box)
 
     return path, visited
 
