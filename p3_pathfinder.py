4519dbb37b5e9ebc75d6f03bcde12e0594b44025
Alec Noble <aljnoble@ucsc.edu>
2015-04-20 15:53:00 -0700

    Reformatting and cleaning

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,50 +1,48 @@
 from heapq import heappush, heappop
 from math import sqrt
 
-def find_path(src, dest, mesh):
 
+def find_path(src, dest, mesh):
     path = []
     visited = []
-    
-    """
-    src_box = dest_box = None
-	
-    for box in mesh['boxes']:
 
-        if src[0] in range(box[0], box[1]):
-            if src[1] in range(box[2], box[3]):
-                src_box = box
-                print 'Source in box ' + str(src_box)
-        if dest[0] in range(box[0], box[1]):
-            if dest[1] in range(box[2], box[3]):
-                dest_box = box
-                print 'Destination in box ' + str(dest_box)
-    """
+    # src_box = dest_box = None
+    #
+    # for box in mesh['boxes']:
+    #
+    # if src[0] in range(box[0], box[1]):
+    # if src[1] in range(box[2], box[3]):
+    # src_box = box
+    #             print 'Source in box ' + str(src_box)
+    #     if dest[0] in range(box[0], box[1]):
+    #         if dest[1] in range(box[2], box[3]):
+    #             dest_box = box
+    #             print 'Destination in box ' + str(dest_box)
+    #
+    #
+    # """Straight line on print-out"""
+    #
+    # path.append((src, dest))
+    #
+    # if (src_box != None):
+    #    visited.append(src_box)
+    # if (dest_box != None):
+    #    visited.append(dest_box)
 
-    """Straight line on print-out"""
-    """
-    path.append((src, dest))
-	
-    if (src_box != None):
-	    visited.append(src_box)
-    if (dest_box != None):
-	    visited.append(dest_box)
-    """
-    
-    #path, visited = bfs(src, dest, mesh, adj_boxes)
-    #path, visited = dijkstras_shortest_path(src, dest, mesh, adj_boxes)
-    #path, visited = a_star(src, dest, mesh, adj_boxes, euclidean_distance)
+    # path, visited = bfs(src, dest, mesh, adj_boxes)
+    # path, visited = dijkstras_shortest_path(src, dest, mesh, adj_boxes)
+    # path, visited = a_star(src, dest, mesh, adj_boxes, euclidean_distance)
     path, visited = bidirectional_a_star(src, dest, mesh, adj_boxes, euclidean_distance)
-    
-    if path == []:
+
+    if not path:
         print("No path")
-    
+
     return path, visited
 
 
 def bfs(source, destination, graph, adj):
     src_box = dest_box = None
-	
+
     for box in graph['boxes']:
         if source[0] in range(box[0], box[1]):
             if source[1] in range(box[2], box[3]):
@@ -53,14 +51,14 @@ def bfs(source, destination, graph, adj):
             if destination[1] in range(box[2], box[3]):
                 dest_box = box
 
-    if src_box == None or dest_box == None:
+    if src_box is None or dest_box is None:
         visited = []
-        if (src_box != None):
+        if src_box is not None:
             visited.append(src_box)
-        if (dest_box != None):
+        if dest_box is not None:
             visited.append(dest_box)
         return [], visited
-        
+
     prev = {}
     detail_points = {}
 
@@ -70,12 +68,12 @@ def bfs(source, destination, graph, adj):
 
     while queue:
         node = heappop(queue)
-        
-        if (node == dest_box):
-            break;
-        
+
+        if node == dest_box:
+            break
+
         neighbors = adj(graph, node)
-        
+
         for next in neighbors:
             if next not in prev:
                 prev[next] = node
@@ -87,27 +85,25 @@ def bfs(source, destination, graph, adj):
     visited = []
     for n in prev:
         visited.append(n)
-    
+
     if node == dest_box:
         path = []
         while prev[node]:
-            """
-            box_center = ((node[1] + node[0])//2, (node[3] + node[2])//2)
-            prev_center = ((prev[node][1] + prev[node][0])//2, (prev[node][3] + prev[node][2])//2)
-            """
+            # box_center = ((node[1] + node[0])//2, (node[3] + node[2])//2)
+            # prev_center = ((prev[node][1] + prev[node][0])//2, (prev[node][3] + prev[node][2])//2)
             path.append((detail_points[node], detail_points[prev[node]]))
             node = prev[node]
         path.append((detail_points[dest_box], destination))
         path.reverse()
-        
+
         return path, visited
     else:
         return [], visited
-    
-    
+
+
 def dijkstras_shortest_path(source, destination, graph, adj):
     src_box = dest_box = None
-	
+
     for box in graph['boxes']:
         if source[0] in range(box[0], box[1]):
             if source[1] in range(box[2], box[3]):
@@ -115,15 +111,15 @@ def dijkstras_shortest_path(source, destination, graph, adj):
         if destination[0] in range(box[0], box[1]):
             if destination[1] in range(box[2], box[3]):
                 dest_box = box
-                
-    if src_box == None or dest_box == None:
+
+    if src_box is None or dest_box is None:
         visited = []
-        if (src_box != None):
+        if src_box is not None:
             visited.append(src_box)
-        if (dest_box != None):
+        if dest_box is not None:
             visited.append(dest_box)
         return [], visited
-        
+
     dist = {}
     prev = {}
     detail_points = {}
@@ -135,18 +131,18 @@ def dijkstras_shortest_path(source, destination, graph, adj):
 
     while queue:
         d, node = heappop(queue)
-        
-        if (node == dest_box):
-            break;
-        
+
+        if node == dest_box:
+            break
+
         neighbors = adj(graph, node)
-        
+
         for next in neighbors:
             constrained_x = max(min(detail_points[node][0], next[1]), next[0])
             constrained_y = max(min(detail_points[node][1], next[3]), next[2])
             next_dist = euclidean_distance((constrained_x, constrained_y), detail_points[node])
             new_cost = dist[node] + next_dist
-            
+
             if next not in prev or new_cost < dist[next]:
                 prev[next] = node
                 dist[next] = new_cost
@@ -156,7 +152,7 @@ def dijkstras_shortest_path(source, destination, graph, adj):
     visited = []
     for n in prev:
         visited.append(n)
-        
+
     if node == dest_box:
         path = []
         while prev[node]:
@@ -171,7 +167,7 @@ def dijkstras_shortest_path(source, destination, graph, adj):
 
 def a_star(source, destination, graph, adj, heuristic):
     src_box = dest_box = None
-	
+
     for box in graph['boxes']:
         if source[0] in range(box[0], box[1]):
             if source[1] in range(box[2], box[3]):
@@ -179,15 +175,15 @@ def a_star(source, destination, graph, adj, heuristic):
         if destination[0] in range(box[0], box[1]):
             if destination[1] in range(box[2], box[3]):
                 dest_box = box
-    
-    if src_box == None or dest_box == None:
+
+    if src_box is None or dest_box is None:
         visited = []
-        if (src_box != None):
+        if src_box is not None:
             visited.append(src_box)
-        if (dest_box != None):
+        if dest_box is not None:
             visited.append(dest_box)
         return [], visited
-    
+
     dist = {}
     prev = {}
     detail_points = {}
@@ -199,12 +195,12 @@ def a_star(source, destination, graph, adj, heuristic):
 
     while queue:
         d, node = heappop(queue)
-        
-        if (node == dest_box):
-            break;
-        
+
+        if node is dest_box:
+            break
+
         neighbors = adj(graph, node)
-        
+
         for next in neighbors:
             constrained_x = max(min(detail_points[node][0], next[1]), next[0])
             constrained_y = max(min(detail_points[node][1], next[3]), next[2])
@@ -212,7 +208,7 @@ def a_star(source, destination, graph, adj, heuristic):
             next_dist = euclidean_distance(next_point, detail_points[node])
             remaining_estimate = heuristic(next_point, destination)
             new_cost = dist[node] + next_dist
-            
+
             if next not in prev or new_cost < dist[next]:
                 prev[next] = node
                 dist[next] = new_cost
@@ -222,7 +218,7 @@ def a_star(source, destination, graph, adj, heuristic):
     visited = []
     for n in prev:
         visited.append(n)
-        
+
     if node == dest_box:
         path = []
         while prev[node]:
@@ -234,10 +230,10 @@ def a_star(source, destination, graph, adj, heuristic):
     else:
         return [], visited
 
-        
+
 def bidirectional_a_star(source, destination, graph, adj, heuristic):
     src_box = dest_box = None
-	
+
     for box in graph['boxes']:
         if source[0] in range(box[0], box[1]):
             if source[1] in range(box[2], box[3]):
@@ -245,20 +241,20 @@ def bidirectional_a_star(source, destination, graph, adj, heuristic):
         if destination[0] in range(box[0], box[1]):
             if destination[1] in range(box[2], box[3]):
                 dest_box = box
-                
-    if src_box == None or dest_box == None:
+
+    if src_box is None or dest_box is None:
         visited = []
-        if (src_box != None):
+        if src_box is not None:
             visited.append(src_box)
-        if (dest_box != None):
+        if dest_box is not None:
             visited.append(dest_box)
         return [], visited
-    
+
     forward_dist = {}
     backward_dist = {}
     forward_prev = {}
     backward_prev = {}
-    detail_points = {} #don't need to duplicate this one since each node will only be found by one search
+    detail_points = {}  # don't need to duplicate this one since each node will only be found by one search
 
     queue = [(0, src_box, 'dest'), (0, dest_box, 'src')]
     forward_dist[src_box] = 0
@@ -267,10 +263,10 @@ def bidirectional_a_star(source, destination, graph, adj, heuristic):
     backward_prev[dest_box] = None
     detail_points[src_box] = source
     detail_points[dest_box] = destination
-    
+
     while queue:
         p, node, goal = heappop(queue)
-        
+
         current_dest = dest_box
         current_dist = forward_dist
         current_prev = forward_prev
@@ -280,12 +276,12 @@ def bidirectional_a_star(source, destination, graph, adj, heuristic):
             current_dist = backward_dist
             current_prev = backward_prev
             other_prev = forward_prev
-        
-        if (node in other_prev):
-            break;
-        
+
+        if node in other_prev:
+            break
+
         neighbors = adj(graph, node)
-        
+
         for next in neighbors:
             constrained_x = max(min(detail_points[node][0], next[1]), next[0])
             constrained_y = max(min(detail_points[node][1], next[3]), next[2])
@@ -293,7 +289,7 @@ def bidirectional_a_star(source, destination, graph, adj, heuristic):
             next_dist = euclidean_distance(next_point, detail_points[node])
             remaining_estimate = heuristic(next_point, current_dest)
             new_cost = current_dist[node] + next_dist
-            
+
             if next not in current_prev or new_cost < current_dist[next]:
                 current_prev[next] = node
                 current_dist[next] = new_cost
@@ -303,38 +299,38 @@ def bidirectional_a_star(source, destination, graph, adj, heuristic):
     visited = []
     for n in forward_prev:
         visited.append(n)
-    for n in backward_prev: #will probably duplicate 1 box
+    for n in backward_prev:  # will probably duplicate 1 box
         visited.append(n)
-        
+
     if node in forward_prev and node in backward_prev:
         path = []
-        
+
         current_node = node
         while forward_prev[current_node]:
             path.append((detail_points[current_node], detail_points[forward_prev[current_node]]))
             current_node = forward_prev[current_node]
-        
+
         current_node = node
         while backward_prev[current_node]:
             path.append((detail_points[current_node], detail_points[backward_prev[current_node]]))
             current_node = backward_prev[current_node]
-        
-        #path.append((detail_points[dest_box], destination))
+
+        # path.append((detail_points[dest_box], destination))
         path.reverse()
         return path, visited
     else:
         return [], visited
-        
-        
+
+
 def adj_boxes(mesh, box):
     adjacent_nodes = mesh['adj'].get(box, [])
     return adjacent_nodes
 
-    
+
 def euclidean_distance(a, b):
     dx = a[0] - b[0]
     dy = a[1] - b[1]
-    return sqrt(dx*dx+dy*dy)
-    
-    
-__author__ = 'Alec'
+    return sqrt(dx * dx + dy * dy)
+
+
+__author__ = 'Alec Noble and Jesse Smith'
